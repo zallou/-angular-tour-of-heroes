d59e5389003f9748168bf722f0a042a916bb4be3
allou zakariae <zakariaeallou@gmail.com>
2018-11-07 00:33:49 +0100

    Display a Heroes List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  hero : Hero;
+  selectedHero: Hero;
+  heroes = HEROES;
   constructor() { }
 
   ngOnInit() {
-      this.hero = {id: 1, name: 'zakariae'};
+     
   }
+  
+  onSelect(hero: Hero): void {
+      this.selectedHero = hero;
+    }
 
 }
